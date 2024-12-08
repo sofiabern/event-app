5b4d64b176121cc18d71fd03d292742b2e171c18
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-08 17:07:15 +0200

    error handler

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,17 +1,31 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-    const events = useLoaderData();
+  const data = useLoaderData();
 
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+  
+  const events = data.events;
 
   return (
     <>
-
       <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    return { isError: true, message: "Could not fetch events" };
+  } else {
+    return response;
+  }
+};
