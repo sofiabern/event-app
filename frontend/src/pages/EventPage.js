9bba7d0c73f6677b9b146dc8657e687891d9ba87
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-08 17:07:06 +0200

    corrected

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -11,12 +11,4 @@ const EventPage = () => {
 
 export default EventPage;
 
-export const loader = async () => {
-  const response = await fetch("http://localhost:8080/events");
 
-  if (!response.ok) {
-  } else {
-    const resData = await response.json();
-    return resData.events;
-  }
-};
