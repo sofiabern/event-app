a2bdac7440fb922a0d35c33fc3caaf55866d319b
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-08 19:19:46 +0200

    error handker 2.0 for react-router-dom < v7

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -7,7 +7,7 @@ const ErrorPage = () => {
   let title = "An error occured!";
   let message = "Something went wrong!";
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
 
   if (error.status === 404) {
